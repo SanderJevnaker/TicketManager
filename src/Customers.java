6a517103352d3e0301d3e9272a9e47165af77094
SanderJevnaker <sanderjevnaker@hotmail.com>
2022-03-07 09:21:52 +0100

    first commit

---
+++ src/Customers.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customers {
+    static final List<Customer> list = new ArrayList<>();
+    static List<Customer> getDummyData() {
+        return list;
+    }
+}
